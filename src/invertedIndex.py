23de17528717da5a53ae53c152869481d634911c
King Fu <xinjian@cs.ualberta.ca>
2021-01-31 12:22:25 -0700

    Updata instructions and comments

--- src/invertedIndex.py
+++ src/invertedIndex.py
@@ -1,22 +1,31 @@
 import json, sys
 
 def error(name):
+    '''
+    Print out the error and exit the program with -1
+    input: name is the name of the error
+    '''
     print(name)
     exit(-1)
 
 if __name__ == "__main__":
+    # Get the arguments and validate the number
     arguments = sys.argv
     if len(arguments) != 3:
         error("Invalid arguents")
+
+    # Open the input file for read and output file for write
     try:
         inputFile = open(arguments[1], 'r')
         outputFile = open(arguments[2], 'w')
     except IOError:
         error('Invalid file arguments')
 
+    # Load and parse json data
     inputData = json.load(inputFile)
     docId = []
     for data in inputData:
+        # Validate doc ID and they are unique
         try:
             ID = int(data['doc_id'])
         except ValueError:
@@ -24,4 +33,8 @@ if __name__ == "__main__":
         if ID in docId:
             error('Invalid Document ID')
         docId.append(ID)
+
+        book = data['book']             # The book name of the document
+        line = data['line']             # The line of the document
+
     print('done')
--- src/query.py
+++ src/query.py
@@ -1,10 +1,17 @@
 import boolean, sys
 
 def error(name):
+    '''
+    Print out the error and exit the program with -1
+    input: name is the name of the error
+    '''
     print(name)
     exit(-1)
 
 if __name__ == "__main__":
+    # Get the arguments and validate the number
     arguments = sys.argv
     if len(arguments) != 3:
         error("Invalid arguents")
+
+    print('done')
